91bfa906da5b9777cffedf637baae4a3a20c1740
Longshadow99 <77301900+Longshadow99@users.noreply.github.com>
2021-01-20 17:31:30 -0500

    Class 1/20

--- Class Participation/GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
+++ Class Participation/GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia,true),
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,true),
+            new Question(R.string.question_africa,true),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true),
+    };
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //ID Finders
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mQuestionTextView =findViewById(R.id.question_text_view);
+        //Click events
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Does Nothing
+                int id = mQuestionBank[0].getmTextResID();
+                boolean truth = mQuestionBank[0].ismAnswerTrue();
+
+                logger.d("The ID is" + id + "and the truth is" + truth);
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Does Nothing
+                Toast.makeText(MainActivity.this,
+                        "You clicked false",
+                        Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
