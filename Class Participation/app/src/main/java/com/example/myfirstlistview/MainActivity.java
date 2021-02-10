d982937d517b9acd8c3648820fa6d744f4186646
Longshadow99 <77301900+Longshadow99@users.noreply.github.com>
2021-02-10 17:37:05 -0500

    End of Class 2/10

--- Class Participation/app/src/main/java/com/example/myfirstlistview/MainActivity.java
+++ Class Participation/app/src/main/java/com/example/myfirstlistview/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.myfirstlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    ListView listView;
+    TextView textView;
+    String[] listItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        textView = findViewById(R.id.textView);
+        listItem = getResources().getStringArray(R.array.array_technology);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.my_list,
+                listItem);
+        listView.setAdapter(adapter);
+
+
+        }
     }
-}
