7326d05e69b77d09a40058e6ee80e9166bf86e5e
Longshadow99 <77301900+Longshadow99@users.noreply.github.com>
2021-02-24 16:05:11 -0500

    Network Image View

---
+++ Class Participation/VolleyTesting/app/src/main/java/com/example/volleytesting/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.volleytesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.my_text);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest =
+                new JsonArrayRequest(Request.Method.GET,
+                        "https://api.androidhive.info/json/movies.json", null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                for (int i = 0; i < response.length(); i++) {
+                                    try {
+                                        JSONObject jsonObjectFromArray =
+                                                response.getJSONObject(i);
+                                        Log.d("JSONArray Response", "Movie Title: " +
+                                                jsonObjectFromArray.getString("title"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("JSONArray Error", "Error:" + error);
+                    }
+                });
+        queue.add(jsonArrayRequest);
+
+    }
+}
